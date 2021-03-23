c69a43c20dd9bdaa506ce56f6c158a1174748df2
NoblePrinceii <farhan.begg@students.makeschool.com>
2021-03-22 21:26:42 -0700

    added cluster map

--- seeds/index.js
+++ seeds/index.js
@@ -21,16 +21,31 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
+    for (let i = 0; i < 200; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             author: '6055b7299ec08390c98529ea',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/483251',
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
-            price
+            price,
+            geometry: {
+                type: "Point",
+                coordinates: [
+                    cities[random1000].longitude,
+                    cities[random1000].latitude
+            ]
+            },
+            images: [
+                {
+                  url: 'https://res.cloudinary.com/dk60n6akt/image/upload/v1616452618/YelpCamp/jwzzjwvkuevegmbimxw9.png',
+                  filename: 'YelpCamp/jwzzjwvkuevegmbimxw9'
+                },{
+                     url: 'https://res.cloudinary.com/dk60n6akt/image/upload/v1616452402/YelpCamp/crktvjyrsposn3qez4bl.png',
+                filename: 'YelpCamp/crktvjyrsposn3qez4bl'
+                 }
+              ]
         })
         await camp.save();
     }
